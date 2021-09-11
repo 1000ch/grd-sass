0ffc36446d04fa2a7183f0e576991b0a4da49f73
1000ch <shogosensui@gmail.com>
2021-09-11 20:11:24 +0900

    refactor: use native ESM

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const test = require('ava');
-const sass = require('sass');
+import fs from 'node:fs';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import test from 'ava';
+import sass from 'sass';
 
 test.cb('can be compiled', t => {
-  const file = path.resolve(__dirname, 'grd.scss')
+  const file = fileURLToPath(new URL('grd.scss', import.meta.url));
   const data = fs.readFileSync(file, {encoding: 'utf-8'});
 
   sass.render({data}, (error, result) => {
